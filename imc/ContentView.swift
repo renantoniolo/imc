a26fdd182cc416ea16449746e7d18f70cbc1ba80
Renan Toniolo Rocha <renantoniolo@gmail.com>
2025-03-18 20:04:09 -0300

    Initial Commit

---
+++ imc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  imc
+//
+//  Created by Renan Toniolo Rocha on 18/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ imc/imcApp.swift
@@ -0,0 +1,17 @@
+//
+//  imcApp.swift
+//  imc
+//
+//  Created by Renan Toniolo Rocha on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct imcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
